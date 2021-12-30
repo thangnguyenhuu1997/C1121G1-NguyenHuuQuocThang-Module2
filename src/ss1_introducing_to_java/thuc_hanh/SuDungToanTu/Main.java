3a70fe01afa3f3bfa66fc63a0fc21bdf643386bf
unknown <you@example.com>
2021-12-30 15:27:51 +0700

    up

---
+++ src/ss1_introducing_to_java/thuc_hanh/SuDungToanTu/Main.java
@@ -0,0 +1,21 @@
+package ss1_introducing_to_java.thuc_hanh.SuDungToanTu;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        float width;
+        float height;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter width: ");
+        width = scanner.nextFloat();
+
+        System.out.println("Enter height: ");
+        height = scanner.nextFloat();
+        float area = width * height;
+
+        System.out.println("Area is: " + area);
+    }
+}
