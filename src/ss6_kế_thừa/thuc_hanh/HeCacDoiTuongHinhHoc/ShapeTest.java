667315e1168c124b2dfdafbe1f5ed9d3275f87bc
unknown <you@example.com>
2022-01-09 15:10:33 +0700

    up

---
+++ src/ss6_kế_thừa/thuc_hanh/HeCacDoiTuongHinhHoc/ShapeTest.java
@@ -0,0 +1,10 @@
+package ss6_kế_thừa.thuc_hanh.HeCacDoiTuongHinhHoc;
+
+public class ShapeTest {
+    public static void main(String[] args) {
+        Shape shape = new Shape();
+        System.out.println(shape);
+        shape = new Shape( "red ",false);
+        System.out.println(shape);
+    }
+}
