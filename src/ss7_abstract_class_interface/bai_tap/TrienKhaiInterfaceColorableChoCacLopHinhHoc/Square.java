cec3b9ec0a32cb98504489f2ac30b35ac783e0c4
unknown <you@example.com>
2022-01-10 22:09:06 +0700

    up

--- src/ss7_abstract_class_interface/bai_tap/TrienKhaiInterfaceColorableChoCacLopHinhHoc/Square.java
+++ src/ss7_abstract_class_interface/bai_tap/TrienKhaiInterfaceColorableChoCacLopHinhHoc/Square.java
@@ -1,7 +1,7 @@
 package ss7_abstract_class_interface.bai_tap.TrienKhaiInterfaceColorableChoCacLopHinhHoc;
 
-public class square extends Graphic{
-    private double canh ;
+public class square extends Graphic {
+    private double canh;
 
     public square(double canh) {
         this.canh = canh;
@@ -14,17 +14,21 @@ public class square extends Graphic{
     public void setCanh(double canh) {
         this.canh = canh;
     }
+
     @Override
     public String toString() {
         return "Square{" +
                 "canh=" + canh +
                 '}';
     }
+
     @Override
     public double getS() {
-        return   Math.pow(canh ,2);
+        return Math.pow(canh, 2);
     }
+
     @Override
     public void howToColor() {
         System.out.println(" Color all four sides.");
     }
+}
